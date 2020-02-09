fa9190f400aabdf1fbda5f904f176cc068e5fad8
Wilson Calil <wilson.calil@outlook.com>
2020-02-08 22:47:30 -0600

    New version of the nearby search, focusing it in four different types of business related to food. It has been included a slider that defines the radius according to the user's choice.

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-
-
-// function initMap() {
-
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//         let lat = position.coords.latitude;
-//         let long = position.coords.longitude;
-//         var latText = parseInt(lat);
-//         var longText = parseInt(long);
-
-//         var location = { lat: latText, lng: longText };
-//         var map = new google.maps.Map(document.getElementById("map"), {
-//             zoom: 4,
-//             center: location
-//         });
-//         var marker = new google.maps.Marker({
-//             position: location,
-//             map: map
-//         });
-//     });
-// }
-
-
-
-
 var map;
 var infowindow;
-var inputVal;
 var idRestaurant;
 
-document.getElementById("myInput").addEventListener("click", function(){
-    inputVal = document.getElementById("val").value;
-    initMap()
-  });
 
-  
-  function initMap() {
-    console.log(inputVal)
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+var userDistance = 16812.4;
+console.log(userDistance)
+
+document.getElementById("myRange").addEventListener("click", function() {
+    userDistance = slider.value * 1609.34
+    initMapPlaces()
+});
+
+
+document.getElementById("btnRest").addEventListener("click", function () {
+
+    initMapPlaces("restaurant")
+});
+
+
+document.getElementById("btnBar").addEventListener("click", function () {
+
+    initMapPlaces("bar")
+});
+
+
+document.getElementById("btnCafe").addEventListener("click", function () {
+
+    initMapPlaces("cafe")
+});
+
+document.getElementById("btnBakery").addEventListener("click", function () {
+
+    initMapPlaces("bakery")
+});
+
+// Initialize function
+
+function initMap() {
 
     navigator.geolocation.getCurrentPosition(function (position) {
         let lat = position.coords.latitude;
@@ -53,50 +62,72 @@ document.getElementById("myInput").addEventListener("click", function(){
             position: currentLocation,
             map: map
         });
+    });
+}
 
-                var request = {
-                location: currentLocation,
-                radius: 18047,
-                types: [inputVal]
-            };
-            infowindow = new google.maps.InfoWindow();
-            var service = new google.maps.places.PlacesService(map);
+// Retrieve restaurants nearby
 
-            service.nearbySearch(request, callback);
+function initMapPlaces(type) {
 
-            function callback(results, status) {
-                if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    for (var i = 0; i < results.length; i++) {
-                        createMarker(results[i]);
-                    }
-                    console.log(results[0].id)
-                    console.log(results)
+    navigator.geolocation.getCurrentPosition(function (position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        var latText = parseFloat(lat);
+        var longText = parseFloat(long);
+
+   
+        var currentLocation = { lat: latText, lng: longText };
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 12,
+            center: currentLocation
+        });
+
+
+        var marker = new google.maps.Marker({
+            position: currentLocation,
+            map: map
+        });
+
+        var request = {
+            location: currentLocation,
+            radius: userDistance,
+            types: [type],
+   
+        };
+        console.log(userDistance)
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+
+        service.nearbySearch(request, callback);
+
+        function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
                 }
+                console.log(results[0].id)
+                console.log(results)
             }
+        }
 
-            function createMarker(place) {
-                var placeLoc = place.geometry.location;
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: place.geometry.location
-                })
-            
+        function createMarker(place) {
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            })
+
 
             google.maps.event.addListener(marker, 'mouseover', function () {
-                //template literal version
-                // infowindow.setContent(`${place.name} <br>${place.vicinity}`)
-                //basic string concatenation version
                 infowindow.setContent(place.name + "<br>" + place.vicinity)
-                // infowindow.setContent(place.vicinity)
                 infowindow.open(map, this)
             });
-        
+
         }
     });
 
 }
 
+
 google.maps.event.addDomListener(window, 'load', initMap)
-
-
-
