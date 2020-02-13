69c845e513a4f455ed3663e13aa130a6c532c485
Wilson Calil <wilson.calil@outlook.com>
2020-02-13 01:33:10 -0600

    development of icons for Google maps, website, delete, and save the restaurant.

--- script.js
+++ script.js
@@ -9,19 +9,25 @@ var selectedRestaurant = document.getElementById("selectedRestaurant");
 var service;
 var slider = document.getElementById("myRange");
 var output = document.getElementById("distance");
+var sliderPrice = document.getElementById("price");
+var outputPrice = document.getElementById("priceIndex");
+var userDistance = 16812.4;
+var zoom;
+
 output.innerHTML = slider.value;
 slider.oninput = function () {
     output.innerHTML = this.value;
-}
-var sliderPrice = document.getElementById("price");
-var outputPrice = document.getElementById("priceIndex");
-outputPrice.innerHTML = sliderPrice.value;
+};
 
+outputPrice.innerHTML = sliderPrice.value;
 sliderPrice.oninput = function () {
     outputPrice.innerHTML = this.value;
-}
-var userDistance = 16812.4;
-console.log(userDistance)
+};
+
+sliderPrice.addEventListener("click", function () {
+    initMapPlaces()
+});
+
 document.getElementById("myRange").addEventListener("click", function () {
     userDistance = slider.value * 1609.34
     initMapPlaces()
@@ -38,6 +44,7 @@ document.getElementById("btnCafe").addEventListener("click", function () {
 document.getElementById("btnBakery").addEventListener("click", function () {
     initMapPlaces("bakery")
 });
+
 // Initialize function
 function initMap() {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -52,12 +59,13 @@ function initMap() {
             center: currentLocation
         });
 
-        var marker = new google.maps.Marker({
+        new google.maps.Marker({
             position: currentLocation,
             map: map
         });
     });
 }
+
 // Retrieve restaurants nearby
 function initMapPlaces(type) {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -67,12 +75,20 @@ function initMapPlaces(type) {
         var latText = parseFloat(lat);
         var longText = parseFloat(long);
         currentLocation = { lat: latText, lng: longText };
+        
+        if (userDistance > 16812.4 ){
+            zoom = 12
+        }
+        else{
+            zoom = 13
+        }
+
         map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 12,
+            zoom: zoom,
             center: currentLocation
         });
 
-        var marker = new google.maps.Marker({
+        new google.maps.Marker({
             position: currentLocation,
             map: map
         });
@@ -83,7 +99,7 @@ function initMapPlaces(type) {
             types: [type],
             // minPriceLevel: 0,
             maxPriceLevel: sliderPrice.value,
-          
+
         };
 
         infowindow = new google.maps.InfoWindow();
@@ -98,73 +114,128 @@ function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
             createMarker(results[i]);
-
         }
-
         randomRestaurant = Math.floor((Math.random() * results.length));
-
-        var paragraph1 = document.createElement("h2");
-        var node1 = document.createTextNode(results[randomRestaurant].name);
-        paragraph1.appendChild(node1);
-        selectedRestaurant.appendChild(paragraph1)
-
-        var paragraph2 = document.createElement("p");
-        var node1 = document.createTextNode("Price Index: " + results[randomRestaurant].price_level);
-        paragraph2.appendChild(node1);
-        selectedRestaurant.appendChild(paragraph2)
-
-        var paragraph3 = document.createElement("p");
-        var node1 = document.createTextNode("Rating: " + results[randomRestaurant].rating);
-        paragraph3.appendChild(node1);
-        selectedRestaurant.appendChild(paragraph3)
-
     }
-
     var requestDetails = {
         placeId: results[randomRestaurant].place_id,
         fields: ['name', 'price_level', 'rating', 'formatted_address', 'icon', 'url', 'photo', 'website', 'formatted_phone_number']
     };
-
     service.getDetails(requestDetails, callbackDetails);
-
 }
-
 
 function callbackDetails(details, status) {
     console.log(details)
     if (status === google.maps.places.PlacesServiceStatus.OK) {
 
+        var idName = JSON.stringify(details.name);
+        var resultsDiv = document.createElement("div")
+        resultsDiv.setAttribute("id", idName);
+        resultsDiv.style.position = 'relative'
+        selectedRestaurant.appendChild(resultsDiv)
+
+        var paragraph1 = document.createElement("h2");
+        var node1 = document.createTextNode(details.name);
+        paragraph1.appendChild(node1);
+        resultsDiv.appendChild(paragraph1)
+        
+        var paragraph2 = document.createElement("p");
+        var node1 = document.createTextNode("Price Level: " + details.price_level);
+        paragraph2.appendChild(node1);
+        resultsDiv.appendChild(paragraph2)
+
+        var paragraph3 = document.createElement("p");
+        var node1 = document.createTextNode("Rating: " + details.rating);
+        paragraph3.appendChild(node1);
+        resultsDiv.appendChild(paragraph3)
+
         var paragraph4 = document.createElement("p");
         var node1 = document.createTextNode("Address: " + details.formatted_address);
         paragraph4.appendChild(node1);
-        selectedRestaurant.appendChild(paragraph4)
-
-        // var webIcon = document.createElement("img");
-        // webIcon.setAttribute('src', 'Google-Logo.png');
-        // document.webIcon.setAttribute(<a href="http://www.addaxtactical.com"><img src='Google-Logo.png'></a>);
-        // {/* /* // webIcon.href = "http://www.cnn.com/";
-        // // webIcon.setAttribute('alt', 'Google Icon'); */
-        // webIcon.setAttribute('height', '50px');
-        // webIcon.setAttribute('width', '50px');
-        // selectedRestaurant.appendChild(webIcon);
-
-
-        var paragraph5 = document.createElement("p");
-        var node1 = document.createTextNode("URL: " + details.url);
-        paragraph5.appendChild(node1);
-        selectedRestaurant.appendChild(paragraph5)
+        resultsDiv.appendChild(paragraph4)
 
         var paragraph6 = document.createElement("p");
         var node1 = document.createTextNode("Phone Number: " + details.formatted_phone_number);
         paragraph6.appendChild(node1);
-        paragraph6.style.borderBottom = "solid";
-        paragraph6.style.paddingBottom = "2%";
-        selectedRestaurant.appendChild(paragraph6)
-   
+        resultsDiv.appendChild(paragraph6)
+
+        var googleIcon = document.createElement("img");
+        googleIcon.setAttribute('src', './assets/Google-Logo.png');
+        googleIcon.setAttribute('height', '50px');
+        googleIcon.setAttribute('width', '50px');
+        googleIcon.setAttribute('alt', 'Google Icon');
+
+        var googleLink = document.createElement("a");
+        googleLink.setAttribute('href', details.url)
+        googleLink.style.position = 'absolute'
+        googleLink.style.right = '200px'
+        googleLink.style.bottom = '20px'
+        googleLink.appendChild(googleIcon)
+        resultsDiv.appendChild(googleLink);
+
+        var webIcon = document.createElement("img");
+        webIcon.setAttribute('src', './assets/Web-Icon.png');
+        webIcon.setAttribute('height', '50px');
+        webIcon.setAttribute('width', '50px');
+        webIcon.setAttribute('alt', 'Web Icon');
+       
+        var webLink = document.createElement("a");
+        webLink.setAttribute('href', details.website)
+        webLink.style.position = 'absolute'
+        webLink.style.right = '140px'
+        webLink.style.bottom = '20px'
+        webLink.appendChild(webIcon)
+        resultsDiv.appendChild(webLink);
+
+        var deleteIcon = document.createElement("img");
+        deleteIcon.setAttribute('src', './assets/trash.png');
+        deleteIcon.setAttribute('height', '50px');
+        deleteIcon.setAttribute('width', '50px');
+        deleteIcon.setAttribute('alt', 'Trash Icon');
+        deleteIcon.style.position = 'absolute'
+        deleteIcon.style.right = '20px'
+        deleteIcon.style.bottom = '20px'
+        resultsDiv.appendChild(deleteIcon);
+
+        var favoriteIcon = document.createElement("img");
+        favoriteIcon.setAttribute('src', './assets/favorites.png');
+        favoriteIcon.setAttribute('height', '50px');
+        favoriteIcon.setAttribute('width', '50px');
+        favoriteIcon.setAttribute('alt', 'Favorite Icon');
+        favoriteIcon.style.position = 'absolute'
+        favoriteIcon.style.right = '80px'
+        favoriteIcon.style.bottom = '20px'
+        resultsDiv.appendChild(favoriteIcon);
+
+        var myScreen = window.matchMedia("(max-width: 700px)")
+        mediaQuery(myScreen)
+        myScreen.addListener(mediaQuery)
+
+        function mediaQuery(myScreen) {
+            if (myScreen.matches) {
+                document.getElementById(idName).style.paddingBottom = "20%";
+            } else {
+                document.getElementById(idName).style.paddingBottom = "5%"
+            }
+        }
+
+        document.getElementById(idName).style.borderBottom = "solid";
+        document.getElementById(idName).style.margingBottom = "5%";
 
     }
     window.scrollTo(0, 10000);
 }
+
+
+    // webLink.addEventListener('click', (selectedRestaurant)=>deleteResuarant(selectedRestaurant))
+    // selectedRestaurant.appendChild(webLink);
+    // }
+    // window.scrollTo(0, 10000);
+    // }
+    // function deleteResuarant(restaurant) {
+    // console.log(restaurant);
+    // alert('deteled')
+
 
 function createMarker(place) {
     var placeLoc = place.geometry.location;
