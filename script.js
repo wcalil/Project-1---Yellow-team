eccdf330ba1d0a680adc41b875000caa8b53f130
Jordon <Jordonrunge20@gmail.com>
2020-02-14 21:15:28 -0600

    Adding the new flex layout for the results

--- script.js
+++ script.js
@@ -131,109 +131,153 @@ function callbackDetails(details, status) {
         var idName = JSON.stringify(details.name);
         var resultsDiv = document.createElement("div")
         resultsDiv.setAttribute("id", idName);
-        resultsDiv.style.position = 'relative'
+        resultsDiv.style.position = 'relative';
+        // resultsDiv.style.display = 'flex';
         selectedRestaurant.appendChild(resultsDiv)
+
+        var infoHead = document.createElement("div");
+            infoHead.setAttribute("class", "infoHead l12");
+
+        var infoText = document.createElement("div");
+            infoText.setAttribute("class", "infoText");
 
         var paragraph1 = document.createElement("h2");
         var node1 = document.createTextNode(details.name);
         paragraph1.appendChild(node1);
-        resultsDiv.appendChild(paragraph1)
+        infoHead.append(paragraph1)
         
         var paragraph2 = document.createElement("p");
         var node1 = document.createTextNode("Price Level: " + details.price_level);
         paragraph2.appendChild(node1);
-        resultsDiv.appendChild(paragraph2)
+        infoText.appendChild(paragraph2)
 
         var paragraph3 = document.createElement("p");
         var node1 = document.createTextNode("Rating: " + details.rating);
         paragraph3.appendChild(node1);
-        resultsDiv.appendChild(paragraph3)
+        infoText.appendChild(paragraph3)
 
         var paragraph4 = document.createElement("p");
         var node1 = document.createTextNode("Address: " + details.formatted_address);
         paragraph4.appendChild(node1);
-        resultsDiv.appendChild(paragraph4)
+        infoText.appendChild(paragraph4)
 
         var paragraph6 = document.createElement("p");
         var node1 = document.createTextNode("Phone Number: " + details.formatted_phone_number);
         paragraph6.appendChild(node1);
-        resultsDiv.appendChild(paragraph6)
+        infoText.appendChild(paragraph6)
 
-        var googleIcon = document.createElement("img");
-        googleIcon.setAttribute('src', './assets/Google-Logo.png');
-        googleIcon.setAttribute('height', '50px');
-        googleIcon.setAttribute('width', '50px');
-        googleIcon.setAttribute('alt', 'Google Icon');
+        resultsDiv.appendChild(infoHead)
 
-        var googleLink = document.createElement("a");
-        googleLink.setAttribute('href', details.url)
-        googleLink.style.position = 'absolute'
-        googleLink.style.right = '200px'
-        googleLink.style.bottom = '20px'
-        googleLink.appendChild(googleIcon)
-        resultsDiv.appendChild(googleLink);
 
-        var webIcon = document.createElement("img");
-        webIcon.setAttribute('src', './assets/Web-Icon.png');
-        webIcon.setAttribute('height', '50px');
-        webIcon.setAttribute('width', '50px');
-        webIcon.setAttribute('alt', 'Web Icon');
-       
-        var webLink = document.createElement("a");
-        webLink.setAttribute('href', details.website)
-        webLink.style.position = 'absolute'
-        webLink.style.right = '140px'
-        webLink.style.bottom = '20px'
-        webLink.appendChild(webIcon)
-        resultsDiv.appendChild(webLink);
+        // console.log(details)
 
-        var deleteIcon = document.createElement("img");
-        deleteIcon.setAttribute('src', './assets/trash.png');
-        deleteIcon.setAttribute('height', '50px');
-        deleteIcon.setAttribute('width', '50px');
-        deleteIcon.setAttribute('alt', 'Trash Icon');
-        deleteIcon.style.position = 'absolute'
-        deleteIcon.style.right = '20px'
-        deleteIcon.style.bottom = '20px'
-        resultsDiv.appendChild(deleteIcon);
+        var photoCard = document.createElement("div")
+            photoCard.setAttribute("class", "card")
+            photoCard.setAttribute("id", "foodCard")
+        var photoCarousel = document.createElement("div")
+            photoCarousel.setAttribute("class", "carousel carousel-slider")
+            photoCarousel.setAttribute("data-indicators", "true")
+            // photoCarousel.setAttribute("id", "demo-carousel")
 
-        var favoriteIcon = document.createElement("img");
-        favoriteIcon.setAttribute('src', './assets/favorites.png');
-        favoriteIcon.setAttribute('height', '50px');
-        favoriteIcon.setAttribute('width', '50px');
-        favoriteIcon.setAttribute('alt', 'Favorite Icon');
-        favoriteIcon.style.position = 'absolute'
-        favoriteIcon.style.right = '80px'
-        favoriteIcon.style.bottom = '20px'
-        resultsDiv.appendChild(favoriteIcon);
-    
-        console.log(details)
-        
+
+        photoCard.appendChild(photoCarousel)
+
         for(i = 0; i <= 4; i++){
             
         var newPhotos = details.photos[i].getUrl({maxWidth: 200, maxHeight: 200})
-        var photoList = document.createElement("ul")
+
+        var imageA = document.createElement("a")
+            imageA.setAttribute("class", "carousel-item")
+            // imageA.setAttribute("href", "#")
+
         var photoIMG = document.createElement("img")
+        
             photoIMG.setAttribute("src", newPhotos)
-            photoList.append(photoIMG)
-            resultsDiv.appendChild(photoList)
+             photoIMG.setAttribute("class", "carouselPhoto")
+            
+            
+            imageA.append(photoIMG)
+            
+            photoCarousel.appendChild(imageA)
+            // photoList.append(photoIMG)
+
+            infoText.appendChild(photoCard)
 
         }
+        $(document).ready(function(){
+            $('.carousel.carousel-slider').carousel({fullWidth: true});
+          });
+
+          resultsDiv.appendChild(infoText)
+          
+          
+        var infoFooter = document.createElement("div")
+            infoFooter.setAttribute("class", "infoFooter")
+
+          var googleIcon = document.createElement("img");
+          googleIcon.setAttribute('src', './assets/Google-Logo.png');
+          googleIcon.setAttribute('height', '50px');
+          googleIcon.setAttribute('width', '50px');
+          googleIcon.setAttribute('alt', 'Google Icon');
+  
+          var googleLink = document.createElement("a");
+          googleLink.setAttribute('href', details.url)
+        //   googleLink.style.position = 'absolute'
+        //   googleLink.style.right = '200px'
+        //   googleLink.style.bottom = '20px'
+          googleLink.appendChild(googleIcon)
+          infoFooter.appendChild(googleLink);
+  
+          var webIcon = document.createElement("img");
+          webIcon.setAttribute('src', './assets/Web-Icon.png');
+          webIcon.setAttribute('height', '50px');
+          webIcon.setAttribute('width', '50px');
+          webIcon.setAttribute('alt', 'Web Icon');
+         
+          var webLink = document.createElement("a");
+          webLink.setAttribute('href', details.website)
+        //   webLink.style.position = 'absolute'
+        //   webLink.style.right = '140px'
+        //   webLink.style.bottom = '20px'
+          webLink.appendChild(webIcon)
+          infoFooter.appendChild(webLink);
+  
+          var deleteIcon = document.createElement("img");
+          deleteIcon.setAttribute('src', './assets/trash.png');
+          deleteIcon.setAttribute('height', '50px');
+          deleteIcon.setAttribute('width', '50px');
+          deleteIcon.setAttribute('alt', 'Trash Icon');
+        //   deleteIcon.style.position = 'absolute'
+        //   deleteIcon.style.right = '20px'
+        //   deleteIcon.style.bottom = '20px'
+          infoFooter.appendChild(deleteIcon);
+  
+          var favoriteIcon = document.createElement("img");
+          favoriteIcon.setAttribute('src', './assets/favorites.png');
+          favoriteIcon.setAttribute('height', '50px');
+          favoriteIcon.setAttribute('width', '50px');
+          favoriteIcon.setAttribute('alt', 'Favorite Icon');
+        //   favoriteIcon.style.position = 'absolute'
+        //   favoriteIcon.style.right = '80px'
+        //   favoriteIcon.style.bottom = '20px'
+          infoFooter.appendChild(favoriteIcon);
+      
+          resultsDiv.append(infoFooter)
 
 
 
 
-        var myScreen = window.matchMedia("(max-width: 700px)")
-        mediaQuery(myScreen)
-        myScreen.addListener(mediaQuery)
+        // var myScreen = window.matchMedia("(max-width: 700px)")
+        // mediaQuery(myScreen)
+        // myScreen.addListener(mediaQuery)
 
-        function mediaQuery(myScreen) {
-            if (myScreen.matches) {
-                document.getElementById(idName).style.paddingBottom = "20%";
-            } else {
-                document.getElementById(idName).style.paddingBottom = "5%"
-            }
-        }
+        // function mediaQuery(myScreen) {
+        //     if (myScreen.matches) {
+        //         document.getElementById(idName).style.paddingBottom = "20%";
+        //     } else {
+        //         document.getElementById(idName).style.paddingBottom = "5%"
+        //     }
+        // }
 
         document.getElementById(idName).style.borderBottom = "solid";
         document.getElementById(idName).style.margingBottom = "5%";
@@ -267,4 +311,9 @@ function createMarker(place) {
         infowindow.open(map, this)
     });
 }
+
+
+
+
+
 google.maps.event.addDomListener(window, 'load', initMap)
